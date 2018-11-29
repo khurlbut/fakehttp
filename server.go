07f98d700bda02c828b422fcf2691e4de2e7d9d9
Kevin Hurlbut <kevinhurlbut@sbcglobal.net>
2018-11-29 15:20:33 -0800

    log the IP and Port to console

--- server.go
+++ server.go
@@ -52,6 +52,7 @@ func (f *HTTPFake) URL() string {
 func listener(ip string, port string) net.Listener {
 	fmt.Println("Modified Version of fakehttp!")
 
+	fmt.Printf("Attempting to listen on %s:%v\n", ip, port)
 	l, err := net.Listen("tcp", ip+":"+port)
 	if err != nil {
 		fmt.Println("--- TCP FAILED! Using TCP6! --- (err: " + err.Error())
